af3e4fc0ca4df428d767108158f924e04173fb9b
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2023-11-02 12:14:01 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import "./App.css";
+import Home from "./pages/Home/Home";
 
 function App() {
  
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Home/>
     </>
   );
 }
---
+++ src/shared/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from "../../assets/images/Logo.svg";
+
+const Navbar = () => {
+    return (
+        <header className='bg-[#1C2B35] h-[80px] flex items-center'>
+            <div className="fullNav flex justify-between container mx-auto align-middle">
+                <div className="img_container">
+                    <img src={logo} alt="" />
+                </div>
+                <div className="navContainer text-white my-auto">
+                    <nav className=''>
+                        <ul className='flex gap-5 '>
+                            <li><a href="">Order</a></li>
+                            <li><a href="">Order Review</a></li>
+                            <li><a href="">Order</a>Manage Inventory</li>
+                            <li><a href="">Order</a>Login</li>
+                            <li><a href="">Order</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Navbar;
